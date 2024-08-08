6723955f708c08a5f03af98f6fc0a721f697644a
Martin Beyer <ac06ujar@uni-leipzig.de>
2024-08-08 12:34:45 +0200

    AAAAA

--- backend/server.js
+++ backend/server.js
@@ -38,16 +38,8 @@ async function run() {
 }
 run().catch(console.dir);
 
-const allowedOrigins = [process.env.FRONTEND_URL2, process.env.FRONTEND_URL];
-app.use(cors({
-  origin: function(origin, callback) {
-    if (!origin || allowedOrigins.indexOf(origin) !== -1) {
-      callback(null, true);
-    } else {
-      callback(new Error('Not allowed by CORS'));//
-    }
-  }
-}));
+
+app.use(cors());
 app.use(express.json());
 
 // Route für das Hochladen von Thumbnails
