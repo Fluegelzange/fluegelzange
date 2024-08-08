b7b976f43ad20a23d66fd75cbede33892c3a3b0b
Martin Beyer <ac06ujar@uni-leipzig.de>
2024-08-08 12:20:57 +0200

    staged lol

--- backend/server.js
+++ backend/server.js
@@ -44,7 +44,7 @@ app.use(cors({
     if (!origin || allowedOrigins.indexOf(origin) !== -1) {
       callback(null, true);
     } else {
-      callback(new Error('Not allowed by CORS'));
+      callback(new Error('Not allowed by CORS'));//
     }
   }
 }));
