7bac03a66ea14c1a0ecf8c22f8dd89131bcb301c
Martin Beyer <ac06ujar@uni-leipzig.de>
2024-08-13 15:08:47 +0200

    logo pfad angepasst

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,7 +1,7 @@
 import React, { useContext, useEffect, useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
+import logo from '../../public/logo512.png'; // Importiere das Logo
 import { AuthContext } from '../App';
-import logo from '../public/logo512.png'; // Importiere das Logo
 import { fetchArticles } from '../services/api';
 import './Header.css';
 
